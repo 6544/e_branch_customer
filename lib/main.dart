daaf05c04e95ee998194e92e2eb6accf3e671300
mo7amedaliEbaid <mo7amedaliebaid@gmail.com>
2023-08-25 18:11:06 +0300

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:e_branch_customer/providers/auth_provider.dart';
 import 'package:e_branch_customer/providers/home_provider.dart';
 import 'package:e_branch_customer/screens/splash_screen.dart';
+import 'package:e_branch_customer/services/fcm_service.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'firebase_options.dart';
 import 'helpers/config.dart';
-
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FCMService().setupFlutterNotifications();
+  FCMService().showFlutterNotification(message);
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  print('Handling a background message ${message.messageId}');
+}
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  if (!kIsWeb) {
+    await FCMService().setupFlutterNotifications();
+  }
   runApp(MyApp());
 }
 
